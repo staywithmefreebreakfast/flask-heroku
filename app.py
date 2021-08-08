f445edd8aa629b9b4234ddafb25a56cfa9bc59b8
staywithmefreebreakfast <88617802+staywithmefreebreakfast@users.noreply.github.com>
2021-08-08 18:59:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,9 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/name')
+def name():
+        return "เจนจิราพร ทองแดง เลขที่ 10 ม.4/10"
+
 if __name__ == "__main__":
     app.run(debug=False)
