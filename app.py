7d1bbd883f40a67698b20640771af53fed8d0597
staywithmefreebreakfast <88617802+staywithmefreebreakfast@users.noreply.github.com>
2021-08-20 09:25:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-        return "เจนจิราพร ทองแดง <br>เลขที่ 10 ม.4/10"
+        return "<font color=red>เจนจิราพร ทองแดง</font> <br>เลขที่ 10 ม.4/10"
 
 if __name__ == "__main__":
     app.run(debug=False)
